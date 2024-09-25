ebabe37982b41b113a9ed65bb40a39d8e2e539f1
Arjun Jadeja <81246797+ArjunJadeja@users.noreply.github.com>
2024-09-26 02:02:24 +0530

    add sample for Revealing style

--- sample/composeApp/src/commonMain/kotlin/com/arjunjadeja/texty/ui/screens/SampleScreen.kt
+++ sample/composeApp/src/commonMain/kotlin/com/arjunjadeja/texty/ui/screens/SampleScreen.kt
@@ -27,6 +27,7 @@ import com.arjunjadeja.texty.design_system.properties.AppDimens.maxWidth
 import com.arjunjadeja.texty.design_system.properties.AppDimens.paddingBig
 import com.arjunjadeja.texty.samples.BasicSample
 import com.arjunjadeja.texty.samples.LoadingSample
+import com.arjunjadeja.texty.samples.RevealingSample
 import com.arjunjadeja.texty.samples.ScrollingSample
 import com.arjunjadeja.texty.samples.TimeKeepingSample
 import com.arjunjadeja.texty.samples.TypingSample
@@ -86,7 +87,7 @@ private fun NormalDisplayStyleSample(
     is DisplayStyle.Basic -> BasicSample(isDemo = isDemo)
     is DisplayStyle.Blinking -> {}
     is DisplayStyle.Fading -> {}
-    is DisplayStyle.Revealing -> {}
+    is DisplayStyle.Revealing -> RevealingSample(isDemo = isDemo)
     is DisplayStyle.Scrolling -> ScrollingSample(isDemo = isDemo)
     is DisplayStyle.Sliding -> {}
     is DisplayStyle.StickAndReveal -> {}
